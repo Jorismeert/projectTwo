82fed7769537c9a2176c7c4e4e54b8601be95a15
Joris Meert <jorismeert@MacBook-Pro-van-Joris-2.local>
2024-08-06 09:57:16 +0200

    update

--- main.py
+++ main.py
@@ -1,20 +1,89 @@
 import os, sys, time, random, json
 
-with open(f'jsonFiles/capitals.json', 'r') as file:
-    topicDict = json.load(file)  
-questionKeys = list(topicDict.keys())
-random.shuffle(questionKeys)
-
-
-for question in range(int(5)):
-    correctAnswer = topicDict[questionKeys[question]]
-    wrongAnswer = list(set(topicDict.values()))
-
-    answerOptions = ( wrongAnswer + [correctAnswer])
-
-#question = (str(self.questions+1) + '. ' +f'{self.questionPhrase} '.capitalize()+ str(questionKeys[question])+ "?\n")
+class QuizLive:
+    def __init__(self, topic, questions, Questionphrase, score=0, attempt=0):
+        self.topic = topic
+        self.questions = questions
+        self.questionPhrase = Questionphrase
+        self.score =score
+        self.attempt = attempt
+    
+    def createQuizLive(self):
+        time.sleep(1)
+        os.system('clear')
+        print(f'Start:  Quiz {self.topic.title()}.')
+        print()
+        time.sleep(2)
+        # import json dictionary
+        with open(f'jsonFiles/{self.topic}.json', 'r') as file:
+            topicDict = json.load(file)  
+            
+        # create questions
+        questionKeys = list(topicDict.keys())
+        random.shuffle(questionKeys)
+        
+        # create random options
+        for question in range(int(self.questions)):
+            correctAnswer = topicDict[questionKeys[question]]
+            wrongAnswer = list(set(topicDict.values()))
+            del wrongAnswer[wrongAnswer.index(correctAnswer)]
+            wrongAnswer = random.sample(wrongAnswer,3)
+            answerOptions = wrongAnswer + [correctAnswer]
+            random.shuffle(answerOptions)
+        # formulate the question
+            query = f'{question+1}. {self.questionPhrase.capitalize()} {questionKeys[question]}?'
+            print(query)
 
         # formulate the options
-    for k in range(len(answerOptions)):
-        print(f'{answerOptions[k]}')
+            answerList = {}
+            validOptions = []
+            for k in range(len(answerOptions)):
+                answerList[f'{chr(65+k)}']=str(answerOptions[k])
+            for bulletPoint, answers in answerList.items():
+                validOptions.append(bulletPoint)
+                print(f'   {bulletPoint}. {answers}')
+        # prompt answer + keep track of score
+            while True:
+                answer = input("\n   Answer: ").strip().upper()  
+                if answer in validOptions:
+                    userInput = answerList[answer.upper()]
+                    # check answer
+                    if userInput.lower() == correctAnswer.lower():
+                        print('   Correct!')
+                        self.score += 1
+                        self.attempt += 1
+                        time.sleep(0.4)
+                        break
+                    else:
+                        print('   Incorrect!')
+                        self.attempt += 1
+                        time.sleep(1)
+                        break
+                else:
+                    print(f"Invalid input. Please enter one of the following: {', '.join(validOptions)}")
+                    break
+            time.sleep(0.4)
+            print(f'        score: {self.score}/{self.attempt}\n')
+            time.sleep(1)
+            
+
+def getQuizLive():
+    answer = input('New quiz (y/n): ').lower()
+    if answer == 'y':
+        topic = input('Choose a topic: ')
+        questions = input('How many questions: ')
+        questionsPhrase = input('Creata a question: ')
+        return(QuizLive(topic, questions, questionsPhrase))
+    else:
+        sys.exit()
+
+
+def main():
+    quiz = getQuizLive()
+    quiz.createQuizLive()
+
+if __name__=="__main__":
+    main()
+
+
 
--- test.py
+++ test.py
@@ -1,18 +1,19 @@
 import os, sys, time, random, json
 
 class QuizLive:
-    def __init__(self, topic, questions, Questionphrase):
+    def __init__(self, topic, questions, Questionphrase, score=0, attempt=0):
         self.topic = topic
         self.questions = questions
         self.questionPhrase = Questionphrase
-        
-
-
-        
+        self.score =score
+        self.attempt = attempt
     
     def createQuizLive(self):
         time.sleep(1)
+        os.system('clear')
         print(f'Start:  Quiz {self.topic.title()}.')
+        print()
+        time.sleep(2)
         # import json dictionary
         with open(f'jsonFiles/{self.topic}.json', 'r') as file:
             topicDict = json.load(file)  
@@ -30,24 +31,46 @@ class QuizLive:
             answerOptions = wrongAnswer + [correctAnswer]
             random.shuffle(answerOptions)
         # formulate the question
-            print()
             query = f'{question+1}. {self.questionPhrase.capitalize()} {questionKeys[question]}?'
             print(query)
 
         # formulate the options
+            answerList = {}
+            validOptions = []
             for k in range(len(answerOptions)):
-                print(f' {chr(65+k)}. {str(answerOptions[k])}', end='  ')
-            print()
-
-        
-
-
-
+                answerList[f'{chr(65+k)}']=str(answerOptions[k])
+            for bulletPoint, answers in answerList.items():
+                validOptions.append(bulletPoint)
+                print(f'   {bulletPoint}. {answers}')
+        # prompt answer + keep track of score
+            while True:
+                answer = input("\n   Answer: ").strip().upper()  
+                if answer in validOptions:
+                    userInput = answerList[answer.upper()]
+                    # check answer
+                    if userInput.lower() == correctAnswer.lower():
+                        print('   Correct!')
+                        self.score += 1
+                        self.attempt += 1
+                        time.sleep(0.4)
+                        break
+                    else:
+                        print('   Incorrect!')
+                        self.attempt += 1
+                        time.sleep(1)
+                        break
+                else:
+                    print(f"Invalid input. Please enter one of the following: {', '.join(validOptions)}")
+                    break
+            time.sleep(0.4)
+            print(f'        score: {self.score}/{self.attempt}\n')
+            time.sleep(1)
+            
 
 def getQuizLive():
-    answer = input('Make a quiz (y/n): ').lower()
+    answer = input('New quiz (y/n): ').lower()
     if answer == 'y':
-        topic = input('Choose a topic:')
+        topic = input('Choose a topic: ')
         questions = input('How many questions: ')
         questionsPhrase = input('Creata a question: ')
         return(QuizLive(topic, questions, questionsPhrase))
@@ -61,9 +84,4 @@ def main():
 
 if __name__=="__main__":
     main()
-
-
-
-
-
 
